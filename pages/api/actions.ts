d47169fc21011187fc1f255980828b83591b666e
david.ren-sq <dren@squareup.com>
2023-07-17 14:21:55 +0800

    Fix issue

--- pages/api/actions.ts
+++ pages/api/actions.ts
@@ -18,8 +18,8 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     console.log('error: ', e);
   } finally {
     console.log('returning response: ', selectedAction);
-
-    axios.post(req.body.response_url, {
+    const reqBody = JSON.parse(req.body.payload) as SlackAction;
+    axios.post(reqBody.response_url, {
       "text": "Oh hey, this is a nifty ephemeral message response from David, and you just selected " + selectedAction,
     }).finally(() => {
       res.status(200).json({selected: selectedAction});
